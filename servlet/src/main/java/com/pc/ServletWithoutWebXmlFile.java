850d940c4fe58adea30f2e8ed391e795e67abef1
nabin <nkbhaie2017@gmail.com>
2023-10-27 09:52:24 -0600

    add some request response object and html form access

--- servlet/src/main/java/com/pc/ServletWithoutWebXmlFile.java
+++ servlet/src/main/java/com/pc/ServletWithoutWebXmlFile.java
@@ -1,6 +1,7 @@
 package com.pc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,5 +17,11 @@ public class ServletWithoutWebXmlFile extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException{
 		System.out.println("I am a servlet program without the use of web.xml file.");
+		
+		// retrieve the user input in html file and display that in browser
+		String userName = req.getParameter("fname");
+		// instantiate the printwriter in the servlet
+		PrintWriter out = res.getWriter();
+		out.println("Your name is " + userName);
 	}
 }
